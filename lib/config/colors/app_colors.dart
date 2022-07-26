b6286621536f036d9c6a7a90cd28f4fef6ef4f76
Hwassef <hassine.wassef@outloook.com>
2022-07-26 03:54:22 -1100

    group colors used in app in single file

---
+++ lib/config/colors/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color darkCyanColor = Color(0xFF0066B8);
+  static const Color darkBlueColor = Color(0xFF0A3C5F);
+  static const Color cyanColor = Color(0xFF00B4EF);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color lightGreyColor = Color(0xFFCCCCCC);
+  static const Color darkGreyColor = Color(0xFF999999);
+  static const Color lightGreenColor = Color(0xFF4FE1BE);
+  static const Color lightBlackColor = Color(0xFF333333);
+  static const Color purpleColor = Color(0xFF7951FF);
+  static const Color lightRedColor = Color(0xFFFF4B70);
+  static const Color lightBlueColor = Color(0xFF119CED);
+  static const Color lightCyanColor = Color(0xFFE2ECF8);
+}
