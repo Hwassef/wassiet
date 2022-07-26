ef879a5d01d472b3e4cb3b2e447cfa5357cee926
Hwassef <hassine.wassef@outloook.com>
2022-07-26 05:04:45 -1100

    add app_routes

---
+++ lib/config/routes/app_routes.dart
@@ -0,0 +1,25 @@
+class AppRoutes {
+  AppRoutes._();
+  static const String homePage = '/homePage';
+  static const String announcementPage = '/announcementPage';
+  static const String sellerProfilePage = '/sellerProfilePage';
+  static const String myAnnouncementPage = '/myAnnouncementPage';
+  static const String addAnnouncementPage = '/addAnnouncementPage';
+  static const String allChatsPage = '/allChatsPage';
+  static const String conversationPage = '/conversationPage';
+  static const String myFavoritesPage = '/myFavoritesPage';
+  static const String myParticipationsPage = '/myParticipationsPage';
+  static const String notificationsPage = '/notificationsPage';
+  static const String aboutAppPage = '/aboutAppPage';
+  static const String usingTermsPage = '/usingTermsPage';
+  static const String contactUsPage = '/contactUsPage';
+  static const String myProfilePage = '/myProfilePage';
+  static const String editMyInformationsPage = '/editMyInformationsPage';
+  static const String reportThisPersonPage = '/reportThisPersonPage';
+  static const String complaintsPage = '/complaintsPagePage';
+  static const String complaintDetailsPage = '/complaintDetailsPage';
+  static const String searchInAnnouncementsPage = '/searchInAnnouncementsPage';
+  static const String filterPage = '/filterPage';
+  static const String settingsPage = '/settingsPage';
+  static const String changeLanguage = '/changeLanguagePage';
+}
