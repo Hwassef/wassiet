7efa25f25bdc9b9839806a5890e0c1bc291798dc
Hwassef <hassine.wassef@outloook.com>
2022-07-26 03:52:04 -1100

    add icons and images used on app and group them in single file

---
+++ lib/config/images/app_images.dart
@@ -0,0 +1,29 @@
+class AppImages {
+  AppImages._();
+  static const String imagesFolderPath = 'assets/images';
+  static const String iconsFolderPath = 'assets/icons';
+  static const String addIcon = '$iconsFolderPath/add_icon.png';
+  static const String appIcon = '$iconsFolderPath/app_icon.png';
+  static const String backIcon = '$iconsFolderPath/back_icon.png';
+  static const String cameraIcon = '$iconsFolderPath/camera_icon.png';
+  static const String clockIcon = '$iconsFolderPath/clock_icon.png';
+  static const String deleteIcon = '$iconsFolderPath/delete_icon.png';
+  static const String editIcon = '$iconsFolderPath/edit_icon.png';
+  static const String filledHeartIcon = '$iconsFolderPath/filled_hear_icon.png';
+  static const String filledStarIcon = '$iconsFolderPath/filled_star_icon.png';
+  static const String filterIcon = '$iconsFolderPath/filter_icon.png';
+  static const String hammerIcon = '$iconsFolderPath/hammer_icon.png';
+  static const String messageIcon = '$iconsFolderPath/message_icon.png';
+  static const String moreIcon = '$iconsFolderPath/more_icon.png';
+  static const String outlinedHearhIcon = '$iconsFolderPath/outlined_heart_icon.png';
+  static const String phoneIcon = '$iconsFolderPath/phone_icon.png';
+  static const String pinIcon = '$iconsFolderPath/pin_icon.png';
+  static const String removeIcon = '$iconsFolderPath/remove_icon.png';
+  static const String searchIcon = '$iconsFolderPath/search_icon.png';
+  static const String shareIcon = '$iconsFolderPath/share_icon.png';
+  static const String tickIcon = '$iconsFolderPath/tick_icon.png';
+  static const String timerIcon = '$iconsFolderPath/timer_icon.png';
+  static const String unstraightStarIcon = '$iconsFolderPath/unstraight_star_icon.png';
+  static const String userDefaultIcon = '$iconsFolderPath/user_default_icon.png';
+  static const String blueLineImage = '$imagesFolderPath/blue_line_image.png';
+}
