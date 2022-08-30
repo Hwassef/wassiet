0c98637b0e3a5bdc74d96439fa80dad5e9e325dc
Hwassef <hassine.wassef@outloook.com>
2022-08-30 03:43:30 -1100

    fix ui issue in add_comment_modal_bottom_sheet

--- lib/app/features/announcement_details/presentation/widgets/add_comment_modal_bottom_sheet.dart
+++ lib/app/features/announcement_details/presentation/widgets/add_comment_modal_bottom_sheet.dart
@@ -22,94 +22,105 @@ class _AddCommentModalBottomSheetState extends State<AddCommentModalBottomSheet>
   @override
   Widget build(BuildContext context) {
     final TextTheme theme = Theme.of(context).textTheme;
-    return Padding(
-      padding: MediaQuery.of(context).viewInsets,
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.9,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 18.0),
-              child: Align(
-                alignment: Alignment.center,
-                child: SizedBox(
-                  width: AppSizes.size42.w,
-                  child: Divider(
-                    height: 4.h,
-                    thickness: 4.h,
-                    color: AppColors.darkGreyColor,
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.only(
+          top: 16.h,
+          bottom: 46.h,
+        ),
+        child: Padding(
+          padding: MediaQuery.of(context).viewInsets,
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 18.0),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      width: AppSizes.size42.w,
+                      child: Divider(
+                        height: 4.h,
+                        thickness: 4.h,
+                        color: AppColors.darkGreyColor,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      S.current.yourRate,
-                      style: theme.headline5?.copyWith(
-                        color: AppColors.blackColor,
-                      ),
-                    ),
-                    RatingBar.builder(
-                      initialRating: 3,
-                      minRating: 1,
-                      direction: Axis.horizontal,
-                      allowHalfRating: true,
-                      itemCount: 5,
-                      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                      itemBuilder: (context, _) => Transform.rotate(
-                        angle: 180 * math.pi / 260,
-                        child: const Icon(
-                          Icons.star,
-                          color: AppColors.cyanColor,
+                Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          S.current.yourRate,
+                          style: theme.headline5?.copyWith(
+                            color: AppColors.blackColor,
+                          ),
                         ),
-                      ),
-                      onRatingUpdate: (rating) {},
+                        RatingBar.builder(
+                          initialRating: 3,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                          itemBuilder: (context, _) => Transform.rotate(
+                            angle: 180 * math.pi / 260,
+                            child: const Icon(
+                              Icons.star,
+                              color: AppColors.cyanColor,
+                            ),
+                          ),
+                          onRatingUpdate: (rating) {},
+                        ),
+                        Observer(
+                          builder: (_) => InputTextField(
+                            controller: commentTextEditingController,
+                            keyboardType: TextInputType.name,
+                            label: S.current.searchForSomething,
+                            focusNode: commentFocusNode,
+                            isRequired: false,
+                            onChanged: (String text) {},
+                            maxLines: 4,
+                            minLines: 4,
+                          ),
+                        ),
+                      ],
                     ),
-                    Observer(
-                      builder: (_) => InputTextField(
-                        controller: commentTextEditingController,
-                        keyboardType: TextInputType.name,
-                        label: S.current.searchForSomething,
-                        focusNode: commentFocusNode,
-                        isRequired: false,
-                        onChanged: (String text) {},
-                        maxLines: 4,
-                        minLines: 4,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                24.0.h.verticalSpace,
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.w),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.white,
+                          spreadRadius: 50,
+                          blurRadius: 40,
+                        ),
+                      ],
+                    ),
+                    child: GradientElevatedButton(
+                      isButtonEnabled: true,
+                      onPressed: () {
+                        context.popRoute();
+                      },
+                      child: Text(S.current.confirm),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 24.0),
-              child: Container(
-                decoration: const BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.white,
-                      spreadRadius: 50,
-                      blurRadius: 40,
-                    ),
-                  ],
-                ),
-                child: GradientElevatedButton(
-                  isButtonEnabled: true,
-                  onPressed: () {
-                    context.popRoute();
-                  },
-                  child: Text(S.current.confirm),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
